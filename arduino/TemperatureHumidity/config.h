30c6355f9789a49888d084142281db18c4500b0e
Don Coleman <dcoleman@chariotsolutions.com>
2019-03-22 20:55:06 -0400

    config

--- arduino/TemperatureHumidity/config.h
+++ arduino/TemperatureHumidity/config.h
@@ -1,9 +1,9 @@
-const char WIFI_SSID[] = "wifi";
-const char WIFI_PASSWORD[] = "password";
+const char WIFI_SSID[] = "";
+const char WIFI_PASSWORD[] = "";
 
-const char MQTT_BROKER[] = "mqtt.chariotday.com";                
+const char MQTT_BROKER[] = "";                
 const int MQTT_PORT = 8883;
-const char MQTT_USER[] = "device00";
-const char MQTT_PASSWORD[] = "secret";
+const char MQTT_USER[] = "";
+const char MQTT_PASSWORD[] = "";
 
-const String DEVICE_ID = "device00";
+const String DEVICE_ID = "";
